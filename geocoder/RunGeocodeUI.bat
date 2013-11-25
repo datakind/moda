f166a80f24e7a41680c4178b4e95f41de97fffd8
dvndrsn <dave@dvndrsn.com>
2013-11-25 00:02:48 -0500

    Corrected typo in RunGeocodeUI.bat (: used instead of ;). Moved RunGeocodeUI.bat to correct path to run successfully.

--- geocoder/RunGeocodeUI.bat
+++ geocoder/RunGeocodeUI.bat
@@ -2,4 +2,4 @@ REM If OutOfMemoryError error is encountered while running the batch script..
 REM ..try increasing the memory allocated (-Xmx1024m) to a larger amount..
 REM ..such as -Xmx2048m 
 
-java -Xmx1024m -classpath .:./lib/jcsv-1.4.0.jar Geocoder.GeocodeUI
+java -Xmx1024m -classpath .;./lib/jcsv-1.4.0.jar Geocoder.GeocodeUI
